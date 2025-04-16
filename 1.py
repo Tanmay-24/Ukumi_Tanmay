91ef3748cf177ff93752ec25d87049e5fed24ec7
tanmay <tanmay2412@outlook.com>
2025-04-17 00:19:22 +0530

    feat: update browser configuration and implement download functionality

--- 1.py
+++ 1.py
@@ -1,29 +1,30 @@
-from browser_use import Agent, Browser, BrowserConfig
+from browser_use import Agent, Browser, BrowserConfig, BrowserContextConfig
 from langchain_openai import ChatOpenAI
 import asyncio
 from dotenv import load_dotenv
 load_dotenv()
-
+import os
 # Configure the browser to connect to your Chrome instance
 browser = Browser(
     config=BrowserConfig(
-        chrome_instance_path="/usr/bin/brave-browser",
-        _force_keep_brower_alive=True
+        chrome_instance_path="/usr/bin/google-chrome",
+        new_context_config=BrowserContextConfig(save_downloads_path="/home/tanmay/Downloads/")
     )
 )
-
-# Create the agent with your configured browser
-agent = Agent(
-    task="go to chatgpt and ask it to write a python script that uses the langchain library to create a simple agent that can browse the web",
+    
+async def run_download():
+	agent = Agent(
+    task="""
+Navigate to https://drive.google.com/file/d/1De1uM11p4Dv7aYvlbDCw2tXTw9Oa3TRh/view?usp=drive_link
+and download and save the mp4
+""",
     llm=ChatOpenAI(model='gpt-4o'),
     browser=browser,
+    use_vision=True,
 )
+	await agent.run(max_steps=25)
+	await browser.close()
 
-async def main():
-    await agent.run()
-
-    input('Press Enter to close the browser...')
-    await browser.close()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+	asyncio.run(run_download())    
